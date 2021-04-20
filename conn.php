59505bc81ddaa4019960e6cf2ae19dac39e4b94b
jesusrlv <jesusrlv@MacBook-Pro-de-jesusrlv.local>
2021-04-20 09:54:17 -0500

    primeros docs

---
+++ conn.php
@@ -0,0 +1,19 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_SERVER_USERNAME', 'jesusrlv_pej');
+define('DB_SERVER_PASSWORD', '');
+define('DB_DATABASE', 'PEJ21');
+
+$conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
+
+?>
+
+<?php
+// define('DB_SERVER', 'localhost');
+// define('DB_SERVER_USERNAME', 'c2cOnf_psic_0307');
+// define('DB_SERVER_PASSWORD', 'ThmfMTk_G6');
+// define('DB_DATABASE','c2PEJ21');
+
+// $conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
+?>
